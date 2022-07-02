4dc116e5a892f434fea68e6bf8349a916a13de76
robert-muriithi <njuguiinirobert@gmail.com>
2022-07-02 16:34:32 +0300

    adding new custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        /*appBar: AppBar(
+          title: const Text('MiCard'),
+          backgroundColor: Colors.blueGrey[900],
+          elevation: 0.0,
+        ),*/
+        body: SafeArea(
+          child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(top: 100.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('images/profile.jpg'),
+                ),
+                SizedBox(height: 10),
+                Text('Robert M',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)
+                ),
+                SizedBox(height: 10),
+                Text('Flutter Developer',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
